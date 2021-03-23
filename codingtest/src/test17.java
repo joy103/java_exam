99824afdfe3a7295f6dd40ef2ed3d20b11366634
joy103 <jang103103@gmail.com.mail>
2021-03-23 23:19:03 +0900

    HashSet

---
+++ codingtest/src/test17.java
@@ -0,0 +1,21 @@
+//찾아라 프로그래밍 마에스터(포켓몬)
+import java.util.*;
+class Solution17 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int n = (int)(nums.length/2);
+        
+        HashSet set = new HashSet();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        
+        if(set.size()>=n) {
+            answer = n;
+        } else {
+            answer = set.size();
+        }
+        
+        return answer;
+    }
+}
