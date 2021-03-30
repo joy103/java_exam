08f376a13cf366e870b91bff96c9af1d747f6b0f
joy103 <jang103103@gmail.com.mail>
2021-03-30 22:44:24 +0900

    Summer/Winter Coding(~2018)

--- codingtest/src/Test20.java
+++ codingtest/src/Test20.java
@@ -7,7 +7,7 @@ class Test20 {
         for(int i=0;i<nlen-2;i++) {
             for(int j=i+1;j<nlen-1;j++) {
                 for(int k=j+1;k<nlen;k++) {
-                    int sum = nums[i]+nums[j]+nums[k]; //배열에서 숫자를 3개씩 골라 합한 후 list에 추가
+                    int sum = nums[i]+nums[j]+nums[k]; //배열에서 숫자를 3개씩 골라 합함
                     if(isPrime(sum)==true) 
                         count++;  //소수인 경우 카운트
                     if(i==nlen-3&&j==nlen-2&&k==nlen-1) //마지막 경우의 수일 경우 break;
